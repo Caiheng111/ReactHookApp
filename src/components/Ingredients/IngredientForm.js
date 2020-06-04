722bbcbae5617fc816b7bbecfed3d10faa92926a
Cai heng <ianyaheng822@gmail.com>
2020-06-04 21:31:14 +1000

    add the database

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,16 +1,15 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
-const IngredientForm = React.memo(props => {
-
- const [enteredTitle, setEnteredTitle] = useState('');
- const [enteredAmount, setEnteredAmount] = useState('');
+const IngredientForm = React.memo(({onAddIngredient}) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
-    
+    onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
@@ -19,23 +18,24 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" 
-            id="title"
-            value={enteredTitle}
-            onChange={(event)=>{
-              setEnteredTitle(event.target.value)
-            }}
-             />
+            <input
+              type="text"
+              id="title"
+              value={enteredTitle}
+              onChange={event => {
+                setEnteredTitle(event.target.value);
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input 
-            type="number" 
-            id="amount" 
-            value={enteredAmount}
-            onChange={(event)=>{
-              setEnteredAmount(event.target.value)
-            }}
+            <input
+              type="number"
+              id="amount"
+              value={enteredAmount}
+              onChange={event => {
+                setEnteredAmount(event.target.value);
+              }}
             />
           </div>
           <div className="ingredient-form__actions">
